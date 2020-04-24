2c758140c8c794b1931a7d4766a3bf726de2f0ff
gregk89 <63820242+gregk89@users.noreply.github.com>
2020-04-24 15:57:29 -0400

    Greg's great additions

--- codenames.js
+++ codenames.js
@@ -1,7 +1,9 @@
 
 // create a new game and put a word in every tile
-const newGame = () => {
+const newGame = (seed) => {
 	let innerHtml = ""
+
+	Math.seedrandom(seed.value);
 
 	let words = getWords();
 
@@ -11,14 +13,15 @@ const newGame = () => {
 	for (i = 0; i < cards.length; i++) {
 		document.getElementById("tile" + i).textContent = cards[i].word;
 	}
+
 }
 
 // change color of tile text based on the color of the card during initalization
 const flipCard = (element) => {
 	card = getCard(element.textContent);
 	docElem = document.getElementById(element.id);
-	if (docElem.style.color != card.color ? 
-		docElem.style.color = card.color : 
+	if (docElem.style.color != card.color ?
+		docElem.style.color = card.color :
 		docElem.style.color = 'black');
 }
 
@@ -32,17 +35,24 @@ const changeColor = (card) => {
 	console.log(card.color);
 }
 
-// get static list of words
+// get random list of words from an array of possible words
 const getWords = () => {
-	return ['banana', 'cat', 'tree', 'table', 'chair', 
-	'wine', 'mouse', 'computer', 'phone', 'book', 
-	'lamp', 'bear', 'window', 'purse', 'house', 
-	'speaker', 'camera', 'key', 'pen', 'eye']
+	let possibleWords = ['banana', 'cat', 'tree', 'table', 'chair',
+	'wine', 'mouse', 'computer', 'phone', 'book',
+	'lamp', 'bear', 'window', 'purse', 'house',
+	'speaker', 'camera', 'key', 'pen', 'eye',
+	'boat', 'snake', 'castle', 'dog', 'monkey',
+	'cloud', 'calendar', 'house', 'lion', 'mountain',
+	'beer', 'snow', 'television', 'farm', 'car'];
+
+	possibleWords.sort(() => Math.random() - 0.5);
+
+	return possibleWords.slice(0,20);
 }
 
 // create random set of 20 word
 const getCards = (words) => {
-	cards = []
+	cards = [];
 	colors = getRandomColors();
 	for (i = 0; i < words.length; i++) {
 		cards.push({"word":words[i], "color":colors[i]})
@@ -67,14 +77,14 @@ const getRandomColors = () => {
 		} else if (i < 15) {
 
 			colors[index] = 'red';
-			
+
 		} else if (i < 19) {
 
 			colors[index] = 'brown';
 		}
 	}
 
-	// once all other colors have been assigned, 
+	// once all other colors have been assigned,
 	// put the assassin in the remaining space
 	for (i = 0; i < 20; i++) {
 		if (!colors[i]) {
@@ -93,5 +103,13 @@ const getNextOpenSpace = (colorsArray, currentIndex, gridSize) => {
 		if (!colorsArray[nextOpenSpace]) {
 			return nextOpenSpace;
 		}
+	}
+}
+
+
+//this function shows the user the answers
+const seeColors = () => {
+	for (i = 0; i < 20; i++) {
+		flipCard(document.getElementById("tile" + i));
 	}
 }
