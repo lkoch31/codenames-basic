b5eb3a5aedd2c7358966c2138aba5208a812474e
Lacy Thomas <lacy.thomas@weatheranalytics.com>
2020-04-24 11:23:05 -0400

    add color change functionality when clicking tile

--- codenames.js
+++ codenames.js
@@ -1,3 +1,5 @@
+
+// create a new game and put a word in every tile
 const newGame = () => {
 	let innerHtml = ""
 
@@ -6,15 +8,28 @@ const newGame = () => {
 	let cards = getCards(words);
 	console.log(cards);
 
-	cards.forEach((card) => {
-		innerHtml += "<div class='grid-item' style='color:" + card.color +"''>" + card.word + "</div>";
-	});
-	console.log(innerHtml);
-
-
-	document.getElementById("board").innerHTML=innerHtml;
+	for (i = 0; i < cards.length; i++) {
+		document.getElementById("tile" + i).textContent = cards[i].word;
+	}
 }
 
+// change color of tile text based on the color of the card during initalization
+const flipCard = (element) => {
+	card = getCard(element.textContent);
+	document.getElementById(element.id).style.color = card.color;
+}
+
+// find the specific card in the cards array by the 'word' value
+const getCard = (word) => {
+	return cards.find(card => card.word === word);
+}
+
+// currently unused....
+const changeColor = (card) => {
+	console.log(card.color);
+}
+
+// get static list of words
 const getWords = () => {
 	return ['banana', 'cat', 'tree', 'table', 'chair', 
 	'wine', 'mouse', 'computer', 'phone', 'book', 
@@ -22,6 +37,7 @@ const getWords = () => {
 	'speaker', 'camera', 'key', 'pen', 'eye']
 }
 
+// create random set of 20 word
 const getCards = (words) => {
 	cards = []
 	colors = getRandomColors();
@@ -37,23 +53,19 @@ const getRandomColors = () => {
 
 	for (i = 0; i < 19; i++) {
 		index = Math.floor(Math.random() * 20);
+		if (colors[index]) {
+			index = getNextOpenSpace(colors, index, 20);
+		}
+
 		if (i < 7) {
 
 			colors[index] = 'blue';
 
 		} else if (i < 15) {
 
-			if (colors[index]) {
-				index = getNextOpenSpace(colors, index, 20);
-			}
-
 			colors[index] = 'red';
 			
 		} else if (i < 19) {
-
-			if (colors[index]) {
-				index = getNextOpenSpace(colors, index, 20);
-			} 
 
 			colors[index] = 'brown';
 		}
